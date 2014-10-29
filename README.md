9a7f026116e48785cf6b2fb7a7cf4a54c659af2c
leiguorui <leiguorui@goodow.com>
2014-10-29 17:30:25 +0800

    sockjs demo

---
+++ README.md
@@ -0,0 +1,7 @@
+## 在IntelliJ IDEA中调试vert.x程序
+
+> 1. Open up the Run configuration
+> 2. I select the Application configuration
+> 3. I specify the main class as : org.vertx.java.deploy.impl.cli.Starter
+> 4. Program arguments : run src/Server.java
+> 5. use classpath of module .. the only module selectable
---
+++ src/main/java/com/renrennet/vertx/SockJSExample.java
@@ -0,0 +1,38 @@
+package com.renrennet.vertx;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.buffer.Buffer;
+import org.vertx.java.core.http.HttpServer;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.core.sockjs.SockJSServer;
+import org.vertx.java.core.sockjs.SockJSSocket;
+import org.vertx.java.platform.Verticle;
+
+public class SockJSExample extends Verticle {
+
+    public void start() {
+        HttpServer server = vertx.createHttpServer();
+
+        server.requestHandler(new Handler<HttpServerRequest>() {
+            public void handle(HttpServerRequest req) {
+                if (req.path().equals("/")) req.response().sendFile("webapp/index.html"); // Serve the html
+            }
+        });
+
+        SockJSServer sockServer = vertx.createSockJSServer(server);
+
+        sockServer.installApp(new JsonObject().putString("prefix", "/testapp"), new Handler<SockJSSocket>() {
+            public void handle(final SockJSSocket sock) {
+                sock.dataHandler(new Handler<Buffer>() {
+                    public void handle(Buffer data) {
+                        sock.write(data); // Echo it back
+                    }
+                });
+            }
+        });
+
+        server.listen(8080);
+    }
+
+}
